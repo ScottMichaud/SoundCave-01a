7cd532c8eada27730b335c63e08650f4d41606e4
Scott Michaud <scott@loosescrew.ca>
2015-06-25 16:30:23 -0400

    Implement Pointerlock and Cursor Hide.

---
+++ Assets/User Assets/UI/IngameUI.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class IngameUI : MonoBehaviour {
+
+	private bool isInMenu;
+
+	// Use this for initialization
+	void Start () {
+		isInMenu = false;
+		LockCursor();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isInMenu) {
+			isInMenu = CrossPlatformInputManager.GetButtonDown ("Open Menu");
+			LockCursor();
+		} else {
+			isInMenu = !CrossPlatformInputManager.GetButtonDown ("Fire1");
+			UnlockCursor();
+		}
+	}
+
+	void LockCursor () {
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+
+	void UnlockCursor () {
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = true;
+	}
+}
